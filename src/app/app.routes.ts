173ac9592fc29c872d6645f15e50f906dce70dab
unknown <felipeherrerapinzon@gmail.com>
2024-03-14 16:18:45 -0500

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { CreateComponent } from './post/create/create.component';
 import { EditComponent } from './post/edit/edit.component';
   
 export const routes: Routes = [
+      { path: '', redirectTo: 'post/index', pathMatch: 'full'},
       { path: 'post', redirectTo: 'post/index', pathMatch: 'full'},
       { path: 'post/index', component: IndexComponent },
       { path: 'post/:postId/view', component: ViewComponent },
